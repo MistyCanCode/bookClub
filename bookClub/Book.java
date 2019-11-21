8cd9d4afb903d08503f463bf9f53b4cb21b9ea76
MistyCanCode <46390494+MistyCanCode@users.noreply.github.com>
2019-11-21 12:18:04 -0600

    Add files via upload
    java project from class

---
+++ bookClub/Book.java
@@ -0,0 +1,15 @@
+//simple book club program
+
+public class Book
+{
+    public static void main(String[] args)
+    {
+        System.out.printIn("Book Title				Author 				Date");
+	System.out.printIn("*******************************************************************************");
+        System.out.printIn("Divine by Mistake                   P.C. Cast			7/21/2009\n");
+	System.out.printIn("Insomnia                            Stephen King                    9/15/1994\n");
+	System.out.printIn("Nineteen Minutes                    Jody Piccoult                   3/9/2007\n");
+	System.out.printIn("Embraced by the Light               Betty Eadie			9/1/1994\n");
+	System.out.printIn("He Still Moves Stones               Max Lucado			8/18/1992\n");
+    }
+}
---
+++ bookClub/bookClub.java
@@ -0,0 +1,15 @@
+//simple book club program
+
+public class Book
+{
+	public static void main(string{} args)
+		{
+            system.out.printIn("Book Title				Author 				Date")
+			system.out.printIn("*****************************************************")
+			system.out.printIn("Divine by Mistake		P.C. Cast			7/21/2009\n")
+			system.out.printIn("Insomnia				Stephen King		9/15/1994\n")
+			system.out.printIn("Nineteen Minutes		Jody Piccoult		3/9/2007\n")
+			system.out.printIn("Embraced by the Light	Betty Eadie			9/1/1994\n")
+			system.out.printIn("He Still Moves Stones	Max Lucado			8/18/1992\n")
+		}
+}
